3f4c40376b50e46c2da790017521a85ecfd1e052
augustovictor <victor.costa@loft.com.br>
2022-10-04 20:31:22 -0300

    feat: Change server response

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world!"))
+		w.Write([]byte("Hello victor!"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
